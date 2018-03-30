c65db046525b87a9034dd062e4995da8120a8319
Apeque <hello@apeque.com>
2018-03-30 16:37:02 -0700

    Update AtoZList.js

--- AtoZList.js
+++ AtoZList.js
@@ -86,7 +86,7 @@ export default class AtoZList extends Component {
             renderCell={this.props.renderCell}
             renderSectionHeader={this.props.renderSection}
             incrementDelay={16}
-            initialNumToRender={8}
+            initialNumToRender={50}
             pageSize={Platform.OS === 'ios' ? 15 : 8}
             maxNumToRender={70}
             numToRenderAhead={40}
